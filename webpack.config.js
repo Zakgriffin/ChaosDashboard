4cf9fc732e26020f00604f0db8f5bdf65e36eeec
ZakGriffin <zakgriffin1209@gmail.com>
2019-11-25 19:51:29 -0500

    fixed App component not working correctly

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = [
     {
-      mode: 'development',
-      entry: './src/electron.ts',
-      target: 'electron-main',
-      module: {
-        rules: [{
-          test: /\.ts$/,
-          include: /src/,
-          use: [{ loader: 'ts-loader' }]
-        }]
-      },
-      output: {
-        path: __dirname + '/dist',
-        filename: 'electron.js'
-      }
+        mode: 'development',
+        entry: './src/electron.ts',
+        target: 'electron-main',
+        module: {
+            rules: [{
+                test: /\.ts(x?)$/,
+                include: /src/,
+                use: [{ loader: 'ts-loader' }]
+            }]
+        },
+        output: {
+            path: __dirname + '/dist',
+            filename: 'electron.js'
+        }
     },
     {
         mode: 'development',
@@ -24,18 +24,21 @@ module.exports = [
         target: 'electron-renderer',
         devtool: 'source-map',
         module: { rules: [{
-          test: /\.ts(x?)$/,
-          include: /src/,
-          use: [{ loader: 'ts-loader' }]
-        }] },
+            test: /\.ts(x?)$/,
+            include: /src/,
+            use: [{loader: 'ts-loader'}]
+        }]},
         output: {
-          path: __dirname + '/dist',
-          filename: 'react.js'
+            path: __dirname + '/dist',
+            filename: 'react.js'
+        },
+        resolve: {
+           extensions: ['.js', '.ts', '.tsx'],
         },
         plugins: [
-          new HtmlWebpackPlugin({
-            template: './src/index.html'
-          })
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            })
         ]
-      }
-  ]
+    }
+]
